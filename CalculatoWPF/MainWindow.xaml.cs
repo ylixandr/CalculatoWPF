647f6391a2aa5ab95e682510629e1cf1e8d6de1b
Ylixandr <savoshinsky55@gmail.com>
2023-10-30 15:09:12 +0300

    Добавьте файлы проекта.

---
+++ CalculatoWPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatoWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach(UIElement el in MainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            
+            if (str == "AC")
+            {
+                TextLabel.Text = "";
+            }
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(TextLabel.Text,null).ToString();
+                TextLabel.Text = value;
+            }
+            else TextLabel.Text += str;
+
+
+        }
+    }
+}
